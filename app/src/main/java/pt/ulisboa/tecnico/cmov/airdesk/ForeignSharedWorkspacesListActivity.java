4397920907c22dd289b658ea1f4306ea10b59fa3
tristaoeast <tristaoeast@gmail.com>
2015-04-04 01:37:47 +0100

    Fixed list bug in Foreign Shared Workspaces

--- app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/ForeignSharedWorkspacesListActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/ForeignSharedWorkspacesListActivity.java
@@ -66,7 +66,7 @@ public class ForeignSharedWorkspacesListActivity extends ActionBarActivity {
         // Get ListView object from xml
         _listView = (ListView) findViewById(R.id.lv_wsList);
         _listView.setAdapter(_wsNamesAdapter);
-        Set<String> wsNames = _prefs.getStringSet(getString(R.string.activity_own_shared_workspaces_list), new HashSet<String>());
+        Set<String> wsNames = _prefs.getStringSet(getString(R.string.activity_foreign_shared_workspaces_list), new HashSet<String>());
         for (String wsName : wsNames) {
             _wsNamesList.add(wsName);
         }
