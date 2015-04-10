ca22f5c6fa5441c50b9956fd6f8b0d95571fb596
marianafpaulo <mariana.f.paulo@hotmail.com>
2015-04-10 14:30:07 +0100

    removed settings option from ReadTextFileActivity

--- app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/ReadTextFileActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/ReadTextFileActivity.java
@@ -123,7 +123,7 @@ public class ReadTextFileActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_read_text_file, menu);
+       // getMenuInflater().inflate(R.menu.menu_read_text_file, menu);
         return true;
     }
 
