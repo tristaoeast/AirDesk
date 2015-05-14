99d1ae53c288f3440b45e7a3aa0781adf34ca4b6
tristaoeast <tristaoeast@gmail.com>
2015-05-14 16:46:47 +0100

    MENSAGEEEEENNNSSSS

--- app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/ReceiveCommTask.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/ReceiveCommTask.java
@@ -2,6 +2,7 @@ package pt.ulisboa.tecnico.cmov.airdesk;
 
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -36,20 +37,14 @@ public class ReceiveCommTask extends AsyncTask<SimWifiP2pSocket, String, Void> {
         //  MESSAGE FORMAT: VIRTIP;COMMAND;ARGS
         try {
             sockIn = new BufferedReader(new InputStreamReader(s.getInputStream()));
-//                while ((st = sockIn.readLine()) != null) {
-//                    publishProgress(st);
-//                }
             st = sockIn.readLine();
             sockIn.close();
             s.close();
-
             String[] splt = st.split(";");
-
             String response = mAppContext.getVirtualIp() + ";";
 
             if (splt[1].equals("WS_SHARED_LIST")) {
-                // VIRTIP;WS_SHARED_LIST;EMAIL
-                //TODO PROVIDE WS_SHARED_LIST_RESPONSE
+                // IP;COM;EMAIL
                 response += "WS_SHARED_LIST_RESPONSE" + ";";
                 String email = splt[2];
                 Set<String> allOwnWS = userPrefs.getStringSet("All Owned Workspaces", new HashSet<String>());
@@ -58,15 +53,43 @@ public class ReceiveCommTask extends AsyncTask<SimWifiP2pSocket, String, Void> {
                     Set<String> invitedUsers = userPrefs.getStringSet(ws + "_invitedUsers", new HashSet<String>());
                     Long quota = userPrefs.getLong(ws + "_quota", -1);
                     if (invitedUsers.contains(email)) {
-                        response += email + ";" + quota.toString() + ";";
+                        response += ws + ";" + quota.toString() + ";";
                     }
                 }
                 new OutgoingCommTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, splt[0], response);
 
             }else if(splt[1].equals("WS_SHARED_LIST_RESPONSE")){
-
+                // IP;COM;WS1;Q1;WS2;Q2;
+                for(int i = 2; i < splt.length; i +=2) {
+                    mAppContext.addInvitedWorkspace(splt[i], Long.parseLong(splt[i+1]));
+                }
+                ActionBarActivity act = mAppContext.getCurrentActivity();
+                if(act instanceof ForeignWorkspacesListActivity) {
+                    ((ForeignWorkspacesListActivity) act).updateLists();
+                }
             } else if (splt[1].equals("WS_SUBSCRIBED_LIST")) {
-                //TODO PROVIDE WS_SUBSCRIBED_LIST_RESPONSE
+                //IP;COM;TAGS...
+                response += "WS_SUBSCRIBED_LIST_RESPONSE" + ";";
+                Set<String> ownPublishedWs = userPrefs.getStringSet("Own Public Workspaces", new HashSet<String>());
+                for(int i = 2 ; i < splt.length; i++) {
+                    for(String ws : ownPublishedWs) {
+                        Set<String> tags = userPrefs.getStringSet(ws + "_tags", new HashSet<String>());
+                        if (tags.contains(splt[i])){
+                            Long quota = userPrefs.getLong(ws+"_quota", -1);
+                            response += ws + ";" + quota.toString() + ";";
+                        }
+                    }
+                }
+                new OutgoingCommTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, splt[0], response);
+            } else if (splt[1].equals("WS_SUBSCRIBED_LIST_RESPONSE")) {
+                // IP;COM;WS1;Q1;WS2;Q2;
+                for(int i = 2; i < splt.length; i +=2) {
+                    mAppContext.addSubscribedWorkspace(splt[i], Long.parseLong(splt[i+1]));
+                }
+                ActionBarActivity act = mAppContext.getCurrentActivity();
+                if(act instanceof ForeignWorkspacesListActivity) {
+                    ((ForeignWorkspacesListActivity) act).updateLists();
+                }
             } else if (splt[1].equals("WS_FILE_LIST")) {
                 //TODO PROVIDE WS_FILE_LIST_RESPONSE
             } else if (splt[1].equals("WS_FILE_READ")) {
